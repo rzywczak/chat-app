f82ee601504201090bc0720607fa329a3792fe90
rzywczak <73279676+rzywczak@users.noreply.github.com>
2022-07-24 21:20:51 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const http = require('http')
+const express = require ('express')
+const socketIO = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+
+app.use(express.static(publicDirectoryPath))
+
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('WebSocket connection')
+    socket.emit('countUpdated', count)
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
+    console.log('Working on port ' + port)
+})
+
