73db7c481ad339ba0410c670844d523cfc1ccdec
Rafal Zywczak <rafal.zywczak@stazysta.comarch.pl>
2022-07-25 15:00:09 +0200

    157 1:54

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,17 @@
-const socket = io()
+const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated', count)
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated', count)
+// })
 
-})
+socket.on("showMessage", (messageClient) => {
+  console.log(messageClient);
+});
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
-})
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message);
+});
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const http = require('http')
-const express = require ('express')
-const socketIO = require('socket.io')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketIO(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.use(express.static(publicDirectoryPath));
 
-app.use(express.static(publicDirectoryPath))
+// let count = 0;
+let welcomeMessage = "Welcome";
 
-let count = 0
+io.on("connection", (socket) => {
+  console.log("WebSocket connection");
 
-io.on('connection', (socket) => {
-    console.log('WebSocket connection')
-    socket.emit('countUpdated', count)
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
-    })
-})
+  socket.emit("showMessage", welcomeMessage);
+  socket.broadcast.emit("showMessage", "A new user join!");
+
+  socket.on("sendMessage", (messageClient) => {
+    io.emit("showMessage", messageClient);
+  });
+});
 
 server.listen(port, () => {
-    console.log('Working on port ' + port)
-})
-
+  console.log("Working on port " + port);
+});
